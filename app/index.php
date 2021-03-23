8cb77b009c1efb177efa0c5131687b30fb2f6015
Franco Garancini <f.garancini@hotmail.com>
2021-03-23 19:32:11 -0300

    push

--- app/index.php
+++ app/index.php
@@ -1,13 +1,1 @@
-<?php
-
-$result = 0;
-
-public function CalcularPotencia($num)
-{
-    return pow($num,2);
-}
-
-for ($i=0; $i < 3; $i++) { 
-    $result = CalcularPotencia($i);
-    echo "$result<br/>";
-}
+hola
