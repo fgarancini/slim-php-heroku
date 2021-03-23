40f281d1b4cab92216fef03093b49c20be8d9568
Franco Garancini <f.garancini@hotmail.com>
2021-03-23 19:58:04 -0300

    push

--- app/index.php
+++ app/index.php
@@ -1,6 +1,9 @@
 <?php
-require "../Ejercicios/ejercicio_12.php"
+require "../Ejercicios/ejercicio_12.php";
 
+$result = CalcularPotencia(4);
+
+echo $result;
 // for ($i=1; $i < 4; $i++) { 
 //     $result = CalcularPotencia($i);
 //     echo "$result<br/>";
