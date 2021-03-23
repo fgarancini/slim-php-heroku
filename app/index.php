d0ea35697568bfae5076236a2981999b92a92426
Franco Garancini <f.garancini@hotmail.com>
2021-03-23 19:26:56 -0300

    Aplicacion 11

--- app/index.php
+++ app/index.php
@@ -1,20 +1,13 @@
 <?php
-use \Psr\Http\Message\ServerRequestInterface as Request;
-use \Psr\Http\Message\ResponseInterface as Response;
 
-require_once '../vendor/autoload.php';
+$result = 0;
 
+public function CalcularPotencia($num)
+{
+    return pow($num,2);
+}
 
-
-$app = new \Slim\App([]);
-
-
-
-$app->get('[/]', function (Request $request, Response $response) {    
-    $response->getBody()->write("GET => Bienvenido!!! ,a SlimFramework -> Soy el Server!");
-    return $response;
-
-});
-
-
-$app->run();
+for ($i=0; $i < 3; $i++) { 
+    $result = CalcularPotencia($i);
+    echo "$result<br/>";
+}
