74130cb186f8896bda7787bce1d8dd322fffe79c
Franco Garancini <f.garancini@hotmail.com>
2021-04-06 21:49:56 -0300

    ejercicio 22 y 23

--- app/login.php
+++ app/login.php
@@ -2,12 +2,12 @@
 include_once "./Ejercicios/Ejercicio21/archivos.php";
 include_once "./Ejercicios/Ejercicio21/usuario.php";
 
-$nombre = $_GET["nombre"];
-$clave = $_GET["clave"];
-$mail = $_GET["mail"];
-$user = new Usuario($nombre,$clave,$mail);
+// $nombre = $_GET["nombre"];
+// $clave = $_GET["clave"];
+// $mail = $_GET["mail"];
+// $user = new Usuario($nombre,$clave,$mail);
 
-// $user = new Usuario("admin","1234","gmail@gmail.com");
+$user = new Usuario("admin","1234","gmail@gmail.com");
 
 $data = Archivos::LeerCsv("usuarios.csv");
 $estado = Usuario::VerificaUsuarioRegistrado($user,$data);
