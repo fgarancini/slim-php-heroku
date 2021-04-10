a6d4b63ddb214b7cba4dc1ccb2be8dee5b910e2e
Franco Garancini <f.garancini@hotmail.com>
2021-04-10 18:02:33 -0300

    img

--- app/registro.php
+++ app/registro.php
@@ -5,7 +5,7 @@ include_once "Ejercicios/Ejercicio23/archivos.php";
 echo "Ejercicio 23 pegale a postman!\n";
 
 $destino = "Ejercicios/Ejercicio23/Usuarios/Fotos".$_FILES["archivo"]["name"];
-move_uploaded_file($_FILES["archivo"]["Fotos"],$destino);
+move_uploaded_file($_FILES["archivo"]["tmp_name"],$destino);
 
 $nombre = $_POST["nombre"];
 $clave = $_POST["clave"];
